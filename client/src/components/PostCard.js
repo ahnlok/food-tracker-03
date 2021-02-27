944a9c8f235b8f4210c018686a56fc327d26796a
Sungpil An <ansungpil1@gmail.com>
2021-02-27 15:34:43 -0500

    intial finish

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,10 +1,12 @@
 import React, { useContext } from "react";
-import { Button, Card, Icon, Label, Image } from "semantic-ui-react";
+import { Button, Card, Icon, Label, Image, Popup } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import moment from "moment";
-import { AuthContext } from '../context/auth';
 
+import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton';
+import MyPopup from '../util/MyPopup';
 
 function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
@@ -32,27 +34,22 @@ function PostCard({
       <Card.Content extra>
         {/* Like Button */}
         <LikeButton user={user} post={{ id, likes, likeCount}} />
+
         {/* Comment Button */}
-        <Button as="div" labelPosition="right" as={Link} to={`./posts'/${id}`}>
-          <Button color="blue" basic>
-            <Icon name="comments"  />
+        <MyPopup
+          content="Comment">
+          <Button as="div" labelPosition="right" as={Link} to={`./posts'/${id}`}>
+            <Button color="blue" basic>
+              <Icon name="comments"  />
+            </Button>
+            <Label as="a" basic color="blue" pointing="left">
+              {commentCount}
+            </Label>
           </Button>
-          <Label as="a" basic color="blue" pointing="left">
-            {commentCount}
-          </Label>
-        </Button>
+        </MyPopup>
 
         {/* Delete Button */}
-        {user && user.username === username && (
-          <Button 
-          as="div" 
-          color="red" 
-          floated="right"
-          onClick={() => console.log('Delete post')}
-          >
-            <Icon name="trash" style={{ margin: 0}} />
-          </Button>
-        )}
+        {user && user.username === username && <DeleteButton postId={id} />}
       </Card.Content>
     </Card>
   );
--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-const { ApolloServer, PubSub } = require('apollo-server')
-const mongoose = require('mongoose');
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
 
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require('./config.js');
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
 const pubsub = new PubSub();
 
 const server = new ApolloServer({
-    typeDefs, 
-    resolvers,
-    // Able access the request body in context
-    context: ({ req }) => ({ req, pubsub })
+  typeDefs,
+  resolvers,
+  // Able access the request body in context
+  context: ({ req }) => ({ req, pubsub }),
 });
 
-mongoose.connect(MONGODB, { useNewUrlParser: true })
-    .then(() => {
-        console.log('MongoDB Connected Successfully');
-        return server.listen({ port: 5000 });
-    })
-    .then(res => {
-        console.log(`Server running at ${res.url}`)
-    });
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB Connected Successfully");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client/build/index.html"));
+// });
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${res.url}`);
+  });
