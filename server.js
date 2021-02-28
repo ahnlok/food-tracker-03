7d219e4afc430e5eff71b01eb7194e5109b982a1
Sungpil An <ansungpil1@gmail.com>
2021-02-28 12:38:38 -0500

    deploying

--- server.js
+++ server.js
@@ -1,44 +1,12 @@
-// const { ApolloServer, PubSub } = require('apollo-server');
-// const mongoose = require('mongoose');
-
-// const typeDefs = require('./graphql/typeDefs');
-// const resolvers = require('./graphql/resolvers');
-// const { MONGODB } = require('./config.js');
-
-// const pubsub = new PubSub();
-
-// const PORT = process.env.PORT || 5000;
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   context: ({ req }) => ({ req, pubsub })
-// });
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-// }
-
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-//   .then(() => {
-//     console.log('MongoDB Connected');
-//     return server.listen({ port: PORT });
-//   })
-//   .then((res) => {
-//     console.log(`Server running at ${res.url}`);
-//   })
-//   .catch(err => {
-//     console.error(err)
-//   })
 require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
 const { ApolloServer, PubSub } = require('apollo-server');
+const mongoose = require('mongoose');
 
-// const app = express();
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config.js');
+
 const pubsub = new PubSub();
-
-// const routes = require("./routes");
 
 const PORT = process.env.PORT || 5000;
 
@@ -48,24 +16,62 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req, pubsub })
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
-app.use(express.static("client/build"));
-
-mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/food-tracker-03", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/food-tracker-03", { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+ })
   .then(() => {
-    console.log("Successfully connected to MongoDB");
+    console.log('MongoDB Connected');
+    return server.listen({ port: PORT });
   })
-  .catch((err) => {
-    console.log("Error connecting to MongoDB", err);
-  });
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch(err => {
+    console.error(err)
+  })
+// require("dotenv").config();
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const { ApolloServer, PubSub } = require('apollo-server');
+
+// const app = express();
+// const pubsub = new PubSub();
+
+// const routes = require("./routes");
+
+// const PORT = process.env.PORT || 5000;
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: ({ req }) => ({ req, pubsub })
+// });
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// app.use(express.static("client/build"));
+
+// mongoose
+//   .connect(process.env.MONGODB_URI || "mongodb://localhost/food-tracker-03", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true,
+//   })
+//   .then(() => {
+//     console.log("Successfully connected to MongoDB");
+//   })
+//   .catch((err) => {
+//     console.log("Error connecting to MongoDB", err);
+//   });
 
 // app.get("/api/config", (req, res) => {
 //   res.json({
@@ -75,10 +81,10 @@ mongoose
 
 // app.use(routes);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client/build/index.html"));
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
