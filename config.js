afedb1eb01a81f0326a34801b48f63c429a9d355
Sungpil An <ansungpil1@gmail.com>
2021-02-28 11:36:48 -0500

    i

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB_URI: 'mongodb+srv://ahnlok:ahnlok123@cluster0.h9erh.mongodb.net/post-data?retryWrites=true&w=majority',
+    MONGODB: 'mongodb+srv://ahnlok:ahnlok123@cluster0.h9erh.mongodb.net/post-data?retryWrites=true&w=majority',
     SECRET_KEY: 'some very secret key'
 }
--- index.js
+++ index.js
@@ -1,41 +1,29 @@
-const { ApolloServer, PubSub } = require("apollo-server");
-const mongoose = require("mongoose");
+const { ApolloServer, PubSub } = require('apollo-server');
+const mongoose = require('mongoose');
 
-const typeDefs = require("./graphql/typeDefs");
-const resolvers = require("./graphql/resolvers");
-const { MONGODB } = require("./config.js");
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config.js');
+
 const pubsub = new PubSub();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.port || 5000;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  // Able access the request body in context
-  context: ({ req }) => ({ req, pubsub }),
+  context: ({ req }) => ({ req, pubsub })
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  
-}
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => {
-  console.log("MongoDB Connected Successfully");
-  return server.listen({ port: PORT });
-})
-.then((res) => {
-  console.log(`Server running at ${res.url}`);
-})
-.catch(err => {
-  console.error(err)
-})
-// app.get("*", (req, res) => {
-  
-//   res.sendFile(path.join(__dirname, "client/build/index.html"));
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Server running on http://localhost:${res.url}`);
-//   });
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch(err => {
+    console.error(err)
+  })
